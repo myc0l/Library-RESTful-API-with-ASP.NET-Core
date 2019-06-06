b0da987f1c908c6aac49f886593c899b7acaedcc
Michael_Macharavanda <michael@laminintech.com>
2019-06-06 16:49:26 +0200

    initial commit

---
+++ Library/Dtos/BookForUpdateDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Dtos
+{
+    public class BookForUpdateDto : BookForManipulationDto 
+    {
+        public override string Description
+        {
+            get
+            {
+                return base.Description;
+            }
+
+            set
+            {
+                base.Description = value;
+            }
+        }
+    }
+}
